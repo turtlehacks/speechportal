3b74abf390d6ab0303fbd270c1819cc14a519680
Ricky Han <rickylqhan@gmail.com>
2017-02-18 12:41:27 -0500

    entry

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,35 @@
-import str from "./content.js";
+// import str from "./content.js";
 
-alert(str);
+
+window.current_panoid = '';
+
+function init() {
+    const init_panoid = "DtaclnuEVvssSuojH8CPpw";
+    setSky(init_panoid);
+}
+
+function setSky(panoid){
+    const sky = document.querySelector('a-sky');
+    sky.setAttribute('src', "http://localhost:5000/panoid/" + panoid);
+    window.current_panoid = panoid;
+}
+
+function updateSky(){
+    fetch("http://localhost:5000/next/" + window.current_panoid)
+    .then(
+      (res) =>  res.json()
+    ).then((json) => {
+        // alert("FUCK")
+      console.log('parsed json', json);
+      // alert(json[0]['panoId']);
+      setSky(json[1]['panoId']);
+    }).catch((ex) => {
+      console.log('parsing failed', ex)
+    })
+
+}
+
+window.onload = function() {
+    init();
+    setInterval(updateSky, 100);
+};
